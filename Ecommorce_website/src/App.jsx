e3788b2c9d060c43555a41db0dfbdb69228c3ae9
Somesh Thombare <someshthombre199@gmail.com>
2026-05-28 13:24:01 +0530

    ecommorce project edit

---
+++ Ecommorce_website/src/App.jsx
@@ -0,0 +1,23 @@
+import HomePage from './Pages/HomePage'
+import Checkout from './Pages/Checkout/Checkout'
+import Orders from './Pages/Orders'
+import Tracking from './Pages/Tracking'
+import {Routes,Route} from "react-router"
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path = "/" element = {<HomePage />} />
+      <Route path = "/checkout" element = {<Checkout/>} />
+      <Route path = "/orders" element = {<Orders />}/>
+      <Route path = "/tracking" element = {<Tracking />}/>
+    </Routes>
+
+    </>
+  )
+}
+
+export default App
