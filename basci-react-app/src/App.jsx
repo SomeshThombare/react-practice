9eacb7c0c680be428b8acf4169ae394b55fa94db
Somesh Thombare <someshthombre199@gmail.com>
2026-06-14 17:27:33 +0530

    folder setup

---
+++ basci-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import "./Product"
+import './App.css'
+import Product from "./Product"
+import "./ProductTab";
+import ProductTab from "./ProductTab";
+
+function App() {
+  return (
+    <>
+     <ProductTab/>   
+    </>
+  )
+}
+
+export default App
---
+++ basci-react-app/src/Price.jsx
@@ -0,0 +1,10 @@
+function Price(){
+    return(
+        <div>
+            <p>oldPrice</p>
+            <p>newPrice</p>
+        </div>
+    );
+}
+
+export default Price;
