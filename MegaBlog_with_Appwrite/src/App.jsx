ca18d0041f526fda1460dfa9f8bdfb98a1e8b75c
Somesh Thombare <someshthombre199@gmail.com>
2026-07-18 19:32:38 +0530

    megablog proect environmentsetuo with appwrite backed servise

---
+++ MegaBlog_with_Appwrite/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+ 
+  console.log(import.meta.env.VITE_APP_APPWRITE_URL)
+  return (
+    <>
+       <h1>Welocme to megablob with appwrite</h1>
+    </>
+  )
+}
+
+export default App
